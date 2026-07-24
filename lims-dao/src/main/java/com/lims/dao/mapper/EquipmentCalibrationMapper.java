3f9ad3379f326dd8ec1ebc528e9ae27b0ff529e8
binbinao <duobinji@gmail.com>
2026-07-24 12:00:20 +0800

    feat(#81): equipment calibration and qualification control
    - V22: Create equipment_calibration table with cert, range, accuracy, result
    - EquipmentCalibration entity
    - EquipmentCalibrationMapper, EquipmentCalibrationService
    - Service includes isCalibrationValid() and getExpiringSoon() for scheduling
    - EquipmentCalibrationController with CRUD and validity check endpoints

---
+++ lims-dao/src/main/java/com/lims/dao/mapper/EquipmentCalibrationMapper.java
@@ -0,0 +1,9 @@
+package com.lims.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lims.model.entity.EquipmentCalibration;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EquipmentCalibrationMapper extends BaseMapper<EquipmentCalibration> {
+}
